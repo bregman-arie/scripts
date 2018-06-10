42f6bcc3b16d15b1fc7ebacf8274496d93dec069
abregman <abregman@redhat.com>
2018-06-10 18:22:37 +0300

    Add script to get latest OVS

--- CI/get_latest_ovs.py
+++ CI/get_latest_ovs.py
@@ -11,13 +11,11 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+from bs4 import BeautifulSoup
 import logging
 import sys
 import re
-
-OVS_TO_OSP = {'2.9': '13',
-              '2': '12',
-              'DEFAULT': '11'}
+import urllib2
 
 
 def setup_logging(debug):
@@ -30,17 +28,20 @@ def setup_logging(debug):
 def main():
     """Main App Function."""
 
-    ovs = sys.argv[1]
-    ver = re.search(r'((\d+\.)+\d+)', ovs).group(1)
-    osp = ''
-    while ver != '':
-        if ver in OVS_TO_OSP:
-            osp = OVS_TO_OSP[ver]
-            ver = ''
-        ver = ver[:-2]
-    if not osp:
-        print(OVS_TO_OSP['DEFAULT'])
-    print(osp)
+    url = 'http://pulp.dist.stage.ext.phx2.redhat.com/content/dist/rhel/server/7/7Server/x86_64/fast-datapath/os/Packages/?C=M;O=A'
+    resp = urllib2.urlopen(url)
+    soup = BeautifulSoup(resp, "lxml", from_encoding=resp.info().getparam('charset'))
+    found = False
+
+    while not found:
+        for link in soup.find_all('a', href=True):
+            rpm = link['href']
+            name = re.search(r'(^[a-zA-z0-9\-]*)\-\d', rpm)
+            if name:
+                if name.group(1) == 'openvswitch':
+                    print(link['href'])
+                    found = True
+                    break
 
 
 if __name__ == '__main__':
