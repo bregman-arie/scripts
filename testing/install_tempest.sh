b0646e7b2968ca3ef5706b39356b6e9924460aa7
abregman <abregman@redhat.com>
2018-08-15 19:08:27 +0300

    Modify install_tempest script

--- testing/install_tempest.sh
+++ testing/install_tempest.sh
@@ -6,4 +6,9 @@ pip install os-testr python-tempestconf
 source $1
 # Run Tempest config
 # 1 is flavor ID (openstack flavor list)
-python /tmp/test/lib/python2.7/site-packages/config_tempest/config_tempest.py compute.flavor_ref 1
+# image_ref and image_ref_alt is the ID of cirros image
+python ~/tobiko_venv/lib/python2.7/site-packages/config_tempest/config_tempest.py compute.flavor_ref 1 scenario.img_dir '/tmp' compute.image_ref 262bd1d3-9e57-4795-9b77-d35a0e39c78f compute.image_ref_alt 262bd1d3-9e57-4795-9b77-d35a0e39c78f
+mkdir etc
+# Put pass in volume and ceilometer methods
+# Remove tempest tests
+tempest run --config-file etc/tempest.conf --regex tobiko
