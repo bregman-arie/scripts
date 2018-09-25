8af6c493e1369ab15481960cf4bbab67af343892
abregman <abregman@redhat.com>
2018-09-25 19:36:37 +0300

    Update heat client script

--- openstack/heat_client.py
+++ openstack/heat_client.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-from keystoneauth1.identity import v3
+from keystoneauth1 import loading
 from keystoneauth1 import session
-from keystoneclient.v3 import client
-auth = v3.Password(auth_url='https://my.keystone.com:5000/v3',
-                   user_id='myuserid',
-                   password='mypassword',
-                   project_id='myprojectid')
+from heatclient import client
+
+loader = loading.get_plugin_loader('password')
+auth = loader.load_from_options(auth_url='',
+                                username='',
+                                password='',
+                                user_domain_name='Default')
+
 sess = session.Session(auth=auth)
 heat = client.Client('1', session=sess)
-heat.stacks.list()
+
+for h in heat.stacks.list():
+    print(h)
