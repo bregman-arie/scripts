e329716127b66ead37fbc37ab6c2be9d6cd92c59
abregman <abregman@redhat.com>
2020-03-31 07:05:35 +0300

    update

--- system/new_dev_system.sh
+++ system/new_dev_system.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+# CentOS 8
 
-sudo dnf install -y virtualenv git gcc git
+set -eux
+
+dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+sudo dnf install -y virtualenv git gcc git docker-ce
 git clone https://github.com/bregman-arie/infraform.git
+systemctl start docker
+systemctl enable docker
+firewall-cmd --zone=public --add-port=8080/tcp
+firewall-cmd --zone=public --add-masquerade --permanent
+sudo firewall-cmd --reload
